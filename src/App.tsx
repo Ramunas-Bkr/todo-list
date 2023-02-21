69180775765adfbf72bf55ebb5bba0fdbaba4e8e
Ramunas <rambek@gmail.com>
2023-02-21 16:11:40 +0200

    Props Type

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Todolist} from './Todolist';
+
+let tasks1 = [
+  {id: 1, title: "CSS", isDone: true},
+  {id: 2, title: "JS", isDone: true},
+  {id: 3, title: "React", isDone: false},
+]
+
+let tasks2 = [
+  {id: 1, title: "Milk", isDone: false},
+  {id: 2, title: "Butter", isDone: true},
+  {id: 3, title: "Beer", isDone: false},
+]
+
+let tasks3 = [
+  {id: 1, title: "Avatar", isDone: false},
+  {id: 2, title: "Titanic", isDone: true},
+  {id: 3, title: "Terminator", isDone: false},
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todolist title="What to learn" tasks={tasks1} />
+      <Todolist title="What to buy" tasks={tasks2}  />
+      <Todolist title="Movies"  tasks={tasks3}  />
     </div>
   );
 }
